98adaea22df2aea01d35fc308f075b7b72ae3490
Travis Gagnon <traviswgagnon@gmail.com>
2016-11-04 22:55:04 -0500

    add listings sidebar based off marker data

--- js/map.js
+++ js/map.js
@@ -1,8 +1,30 @@
 //Model Data
-var initialLocations = [
-		{title: "Fercho YMCA", location : {lat : 46.850461,lng: -96.85245200000001}},
-		{title: "Schlossman YMCA", location : {lat : 46.87261849999999, lng : -96.786259}}
-	];
+var markers = [];
+
+  	//Takes results from Google Text search and creates
+	function gymMarkers(results){
+		for (var i = 0; i < results.length; i++) {
+			var marker = new google.maps.Marker({
+				map: map,
+				position: results[i].geometry.location,
+				title: results[i].name
+			});
+			marker.address = results[i].formatted_address;
+			markers.push(marker);
+
+			marker.addListener('click', function() {
+            	createInfoWindow(this, largeInfowindow);
+          	});
+        }
+        console.log(results);
+        createListings();
+	};
+
+
+
+
+
+
 
 var map;
 
@@ -12,32 +34,17 @@ function initMap() {
 	  zoom: 13
 		});
 
-	var largeInfowindow = new google.maps.InfoWindow();
+	largeInfowindow = new google.maps.InfoWindow();
 
 	var gymLocations = {
   	  location: map.center,
-  	  radius: '5000',
+  	  radius: '20000',
    	  query: 'gym'
- 	 };
+ 	};
 
  	service = new google.maps.places.PlacesService(map);
   	service.textSearch(gymLocations, gymMarkers);
-
-	function gymMarkers(results){
-		console.log(results)
-		for (var i = 0; i < results.length; i++) {
-			var marker = new google.maps.Marker({
-				map: map,
-				position: results[i].geometry.location,
-				title: results[i].name
-			});
-			marker.address = results[i].formatted_address;
-
-			marker.addListener('click', function() {
-            	createInfoWindow(this, largeInfowindow);
-          	});
-		}
-	};
+};
 
 	function createInfoWindow(clickedmarker, infowindow){
 		var inforWindowContent = "<div><b>" + clickedmarker.title + "</b></div><br><div>" + clickedmarker.address + "</div>"
@@ -46,6 +53,13 @@ function initMap() {
 			infowindow.setContent(inforWindowContent);
 			infowindow.open(map, clickedmarker);
 
+		};
+	};
+
+	function createListings() {
+		for (var i = 0; i < markers.length; i++) {
+			var listingsContent = "<div><h3>" + markers[i].title + "</h2>" + markers[i].address + "</div><hr>";
+			$('#listings').append(listingsContent);
 		}
-	}
-};
+	};
+
