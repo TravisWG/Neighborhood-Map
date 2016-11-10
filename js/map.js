b21c73b63d6653afc1ebf4e9f664d64c3ba9abd6
Travis Gagnon <traviswgagnon@gmail.com>
2016-11-09 20:41:28 -0600

    Add filter by distance functionality

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 //Model Data
 var markers = [];
-
+var userLocation;
 
 
 
@@ -19,8 +19,8 @@ var markers = [];
             	createInfoWindow(this, largeInfowindow);
           	});
         }
-        console.log(results);
-        createListings();
+        console.log(markers);
+        createListings(markers);
 	};
 
 
@@ -47,6 +47,7 @@ function initMap() {
 
  	service = new google.maps.places.PlacesService(map);
   	service.textSearch(gymLocations, gymMarkers);
+
 };
 
 	function createInfoWindow(clickedmarker, infowindow){
@@ -59,10 +60,79 @@ function initMap() {
 		};
 	};
 
-	function createListings() {
+	function createListings(markers) {
+		$('#listings').empty();
 		for (var i = 0; i < markers.length; i++) {
 			var listingsContent = "<div><h3>" + markers[i].title + "</h3>" + markers[i].address + "</div><hr>";
 			$('#listings').append(listingsContent);
 		}
 	};
 
+    function hideMarkers(markerList) {
+        for (var i = 0; i < markerList.length; i++) {
+          markerList[i].setMap(null);
+        }
+    };
+
+     function setMarkers(markerList) {
+        console.log(markerList);
+        for (var i = 0; i < markerList.length; i++) {
+          markerList[i].setMap(map);
+        }
+    };
+
+	function withinDistance() {
+    	var service = new google.maps.DistanceMatrixService();
+		var userLocation = document.getElementById('location').value;
+		if (userLocation == ""){
+			window.alert('A starting location is required.')
+		} else{
+			hideMarkers(markers);
+			var gymDestinations = [];
+			for (var i = 0; i < markers.length; i++){
+				gymDestinations.push(markers[i].address)
+			}
+		console.log(gymDestinations)
+
+		service.getDistanceMatrix({
+	    	origins: [userLocation],
+	    	destinations: gymDestinations,
+	    	unitSystem: google.maps.UnitSystem.IMPERIAL,
+	    	travelMode: 'WALKING',
+	  	}, function(response, status) {
+            if (status !== google.maps.DistanceMatrixStatus.OK) {
+              window.alert('Error was: ' + status);
+            } else {
+              	filterByDistance(response);
+            }
+          });
+        }
+    }
+
+	function filterByDistance(response){
+		console.log(response)
+		var withinDistance = document.getElementById('max-miles').value;
+		console.log(withinDistance)
+		var results = response.rows[0].elements;
+		var filteredMarkers = []
+		for (var i = 0; i < results.length; i++) {
+			var meters = results[i].distance.value;
+			var miles = metersToMiles(meters);
+			if (miles <= withinDistance) {
+				markers[i]
+				filteredMarkers.push(markers[i])
+			}
+		}
+		setMarkers(filteredMarkers);
+		createListings(filteredMarkers);
+	}
+
+	//converts meters to miles, rounding to the tenth
+	function metersToMiles(numberMeters){
+		var toMileConversion = 0.000621371
+		var miles = numberMeters * toMileConversion
+		var miles = Math.round(miles * 10)/10
+		return miles
+	}
+
+	console.log(metersToMiles(381778))
