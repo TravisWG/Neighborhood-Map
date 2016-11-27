83c27998bafe0e2ecd9a80b3bc064f0957138f5d
Travis Gagnon <traviswgagnon@gmail.com>
2016-11-27 00:28:05 -0600

    add knockout functionality, created marker objects

---
+++ js/helper.js
@@ -0,0 +1,24 @@
+var bikeDiv = '<h4>Select a  biking speed:</h4>' +
+				'<select id="speed">' +
+					'<h4>Select a speed:</h4>' +
+					'<option value="10">10 MPH</option>' +
+					'<option value="12">12 MPH</option>' +
+					'<option value="14">14 MPH</option>' +
+					'<option value="16">16 MPH</option>' +
+					'<option value="18">18 MPH</option>' +
+					'<option value="20">20 MPH</option>' +
+					'<option value="22">22 MPH</option>' +
+					'<option value="24">24 MPH</option>' +
+				'</select>';
+
+var runDiv = '<h4>Select a running speed:</h4>' +
+				'<select id="speed">' +
+					'<option value="5">5 MPH</option>' +
+					'<option value="6">6 MPH</option>' +
+					'<option value="7">7 MPH</option>' +
+					'<option value="8">8 MPH</option>' +
+					'<option value="9">9 MPH</option>' +
+					'<option value="10">10 MPH</option>' +
+					'<option value="11">11 MPH</option>' +
+					'<option value="12">12 MPH</option>' +
+				'</select>';
